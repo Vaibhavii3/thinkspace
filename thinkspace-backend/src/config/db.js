cf96a9bec8ede696d509ba49b609dad85902876b
Vaibhavi <vaibhavichaudhari304@gmail.com>
2024-12-20 20:36:47 +0530

    changed

--- thinkspace-backend/src/config/db.js
+++ thinkspace-backend/src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectDB = async () => {
-        mongoose.connect(process.env.MONGODB_URI)
+        mongoose.connect(process.env.MONGO_URI)
                 .then(console.log("DB Connected Successfully"))
                 .catch( (error) => {
                         console.log("DB Facing Connection Issues");
