b860efc632c5bc6a455e98419918fd5d0ab010ab
Vaibhavi <vaibhavichaudhari304@gmail.com>
2025-06-20 19:04:45 +0530

    cors

--- thinkspace-backend/server.js
+++ thinkspace-backend/server.js
@@ -22,8 +22,11 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-// app.use(cors({ origin: "http://thinkspace-seven.vercel.app"}));
-app.use(cors());
+app.use(cors({ 
+    origin: "https://thinkspace-mu.vercel.app/",
+    credentials: true,
+}));
+// app.use(cors());
 app.use(bodyParser.json());
 
 
